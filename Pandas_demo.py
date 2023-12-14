d86b9924fa9cff24bb699455ff32f5fda01ec509
Jan Hirschner <jan.hirschner@gmail.com>
2023-12-14 19:43:58 +0200

    Test

--- Pandas_demo.py
+++ Pandas_demo.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import random
+from faker import Faker
 import matplotlib.pyplot as plt
+
+fake = Faker()
+
 # Create a DataFrame with 100 entries
 data = {'Name': [],
-        'Age': [],
-        'City': []}
+    'Age': [],
+    'City': []}
 
 for _ in range(100):
-    name = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=5))
+    name = fake.first_name()
     age = random.randint(20, 40)
-    city = random.choice(['New York', 'London', 'Paris', 'Tokyo'])
+    city = random.choice(['New York', 'London', 'Paris', 'Tokyo', 'Prague'])
     data['Name'].append(name)
     data['Age'].append(age)
     data['City'].append(city)
@@ -19,7 +23,7 @@ df = pd.DataFrame(data)
 # Display the DataFrame
 print("\nDataFrame:")
 print(df)
-
+ 
 while True:
     user_input = input("\nType 'next' to continue or 'exit' to end the demo: ")
     if user_input == 'next':
@@ -31,30 +35,49 @@ print("DataFrame:")
 print(df)
 plt.show()
 
+while True:
+    user_input = input("\nType 'next' to continue or 'exit' to end the demo: ")
+    if user_input == 'next':
+        break
+    elif user_input == 'exit':
+        exit()
+
 # Show basic statistics of the DataFrame
 print("\nBasic Statistics:")
 print(df.describe())
+
+while True:
+    user_input = input("\nType 'next' to continue or 'exit' to end the demo: ")
+    if user_input == 'next':
+        break
+    elif user_input == 'exit':
+        exit()
 
 # Filter the DataFrame
 print("\nFiltered DataFrame:")
 filtered_df = df[df['Age'] > 26]
 print(filtered_df)
 
+while True:
+    user_input = input("\nType 'next' to continue or 'exit' to end the demo: ")
+    if user_input == 'next':
+        break
+    elif user_input == 'exit':
+        exit()
+
 # Group the DataFrame by a column
 print("\nGrouped DataFrame:")
-grouped_df = df.groupby('City').count()
+grouped_df = df.groupby('Name').count()
 print(grouped_df)
+
+while True:
+    user_input = input("\nType 'next' to continue or 'exit' to end the demo: ")
+    if user_input == 'next':
+        break
+    elif user_input == 'exit':
+        exit()
 
 # Plot a bar chart of the grouped DataFrame
 print("\nBar Chart:")
 grouped_df.plot(kind='bar')
 plt.show()
-
-# Continue the demo or exit
-while True:
-    user_input = input("\nType 'next' to continue or 'exit' to end the demo: ")
-    if user_input == 'next':
-        # Add more functionalities here
-        pass
-    elif user_input == 'exit':
-        break
