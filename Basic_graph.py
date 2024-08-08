7759e31b79829104bfcbc8f0264aa2bba31bfbd5
Hirsha <jan.hirschner@gmail.com>
2024-08-08 11:16:42 +0200

    Msg

--- Basic_graph.py
+++ Basic_graph.py
@@ -2,14 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-10, 10, 100)
-y = 1 / np.tanh(x)
-
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('coth(x)')
-plt.title('Graph of Hyperbolic Cotangent Function')
-plt.grid(True)
-plt.show()
+y = np.tan(x)
 
 plt.plot(x, y)
 plt.xlabel('x')
