5181d6ffc8e66d6725344c0f43125491f2793c6b
Hirsha <jan.hirschner@gmail.com>
2024-08-08 11:14:58 +0200

    Test

--- Basic_graph.py
+++ Basic_graph.py
@@ -9,14 +9,11 @@ plt.xlabel('x')
 plt.ylabel('coth(x)')
 plt.title('Graph of Hyperbolic Cotangent Function')
 plt.grid(True)
-<<<<<<< HEAD
 plt.show()
 
 plt.plot(x, y)
 plt.xlabel('x')
-plt.ylabel('tanh(x)')
-plt.title('Graph of Hyperbolic Tangent Function')
+plt.ylabel('tan(x)')
+plt.title('Graph of Tangent Function')
 plt.grid(True)
-=======
->>>>>>> b9df6e35e3a4593cc2d8a79e12a459220ac5e08e
 plt.show()
