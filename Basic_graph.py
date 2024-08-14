463214815952408ff67dffeecc7896428a3a61ea
Hirsha <jan.hirschner@gmail.com>
2024-08-14 10:32:39 +0200

    Test

--- Basic_graph.py
+++ Basic_graph.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(-10, 10, 100)
-y = np.tan(x)
+y = np.tanh(x)
 
 plt.plot(x, y)
 plt.xlabel('x')
 plt.ylabel('tan(x)')
-plt.title('Graph of Tangent Function')
+plt.title('Graph of Hyperbolic Tangent Function')
 plt.grid(True)
 plt.show()
