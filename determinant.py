ce2388027290d0aa335beeef2045127ab1c4a7fc
Hirsha <jan.hirschner@gmail.com>
2024-11-14 11:00:30 +0100

    A

--- determinant.py
+++ determinant.py
@@ -1,13 +1,16 @@
+from matplotlib.pylab import rand
 import numpy as np
 
 def main():
     # Define and initialize a 3x3 matrix
-    matrix = np.array([[1.0, 1.0, 3.0],
-                       [4.0, 5.0, -5.0],
-                       [7.0, 8.0, 9.0]])
+    matrix = np.array([[rand(), rand(), rand()],
+                       [rand(), rand(), rand()],
+                       [rand(), rand(), rand()]])
 
     # Compute the determinant
     det = np.linalg.det(matrix)
+
+    print(matrix)
 
     # Print the determinant
     print(f"Determinant of the matrix is: {det}")
