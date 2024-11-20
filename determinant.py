a4a872bf372581ea83c02555c594f12516dd2b9d
Hirsha <jan.hirschner@gmail.com>
2024-11-20 09:18:16 +0100

    A

--- determinant.py
+++ determinant.py
@@ -15,5 +15,13 @@ def main():
     # Print the determinant
     print(f"Determinant of the matrix is: {det}")
 
+    matrix2 = np.full((2, 2, 2), 4)
+    print(matrix2)
+    det2 = np.linalg.det(matrix2)
+    print(f"Determinant of the matrix is: {det2}")
+
+    matrix3 = np.array([[[1,2],[1,2]],[[1,2],[1,2]]])
+    print(matrix3)
+
 if __name__ == "__main__":
     main()
